40d807c7a7db24040cd1e0feac187361ca8431b4
thugtech97 <juhde.1997@gmail.com>
2026-01-05 12:42:14 +0800

    oh yeah

--- pages/files/index.tsx
+++ pages/files/index.tsx
@@ -1,12 +1,30 @@
 import AdminLayout from '@/components/Layout/AdminLayout'
 import FileManagerUI from '@/components/UI/FileManager'
 
-
 export default function FileManagerPage() {
   return (
     <div className="container">
       <h3 className="mb-3">File Manager</h3>
-      <FileManagerUI/>
+      <div
+        style={{
+          width: "100%",
+          height: "75vh",
+          border: "1px solid #dee2e6",
+          borderRadius: 6,
+          overflow: "hidden",
+          background: "#fff",
+        }}
+      >
+        <iframe
+          src={`${process.env.NEXT_PUBLIC_API_URL}/admin/file-manager-ui`}
+          style={{
+            width: "100%",
+            height: "100%",
+            border: "none",
+          }}
+          loading="lazy"
+        />
+      </div>
     </div>
   )
 }
