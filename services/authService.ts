27941c78beff1fca76918f64cdb19e79c51b2e89
thugtech97 <juhde.1997@gmail.com>
2026-03-05 15:04:55 +0800

    auth fix

--- services/authService.ts
+++ services/authService.ts
@@ -8,6 +8,11 @@ export const login = async (email: string, password: string) => {
 
   // 🔥 SAVE TOKEN
   if (response.data?.token) {
+
+    localStorage.removeItem("cms4.currentUser.v1");
+    localStorage.removeItem("cms4.websiteSettings.v1");
+    localStorage.removeItem("cms4.homeBanner.fonts.v1");
+
     localStorage.setItem("auth_token", response.data.token);
   }
 
